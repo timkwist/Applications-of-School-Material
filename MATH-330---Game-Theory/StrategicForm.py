07ae422e46d84bc439e2072062ebdb1bd5142623
Tim Kwist <timkwist@hotmail.com>
2016-02-05 22:49:26 -0800

    Add PrintMatrix

--- MATH-330---Game-Theory/StrategicForm.py
+++ MATH-330---Game-Theory/StrategicForm.py
@@ -8,6 +8,14 @@ class StrategicForm:
 	def getPayoff(self, pOneStrat, pTwoStrat):
 		return self.matrix[pOneStrat][pTwoStrat]
 
+	def printMatrix(self):
+		for key, value in self.matrix.items():
+			for keyTwo, valueTwo in value.items():
+				print("%s:%s --> %s" % (key, keyTwo, valueTwo))
+
+	
+
 prisonersDilemma = StrategicForm()
 prisonersDilemma.addEntry("Testify", "Testify", [-2, -2])
 print(prisonersDilemma.getPayoff("Testify", "Testify"))
+prisonersDilemma.printMatrix()
